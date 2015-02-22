bc481be35dd4e9d1fa0165269625b3579a4030d7
atikenny <atikenny@gmail.com>
2015-02-22 21:03:36 +0100

    allow team manipulation for users

--- tablehockey.js
+++ tablehockey.js
@@ -778,4 +778,16 @@ if (Meteor.isServer) {
       return !!userId;
     }
   });
+
+  Teams.allow({
+    insert: function (userId) {
+      return !!userId;
+    },
+    update: function (userId) {
+      return !!userId;
+    },
+    remove: function (userId) {
+      return !!userId;
+    }
+  });
 }
